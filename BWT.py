25d4ce4ec2a4023c503b498ea183e558cda9458c
Leonardo <mirandolaleonardo1999@gmail.com>
2021-02-12 22:16:27 +0100

    End of day

--- BWT.py
+++ BWT.py
@@ -1,2 +1,55 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import os
+
+def combinations_recurs(sequence, combinations):
+    print(len(combinations))
+    if len(combinations) < 2:
+        len_seq = len(sequence) - 1
+        combinations.append(sequence)
+        combinations.append(sequence[len_seq] + sequence[:len_seq])
+        print(combinations)
+    while len(sequence) != len(combinations):
+        process = sequence.split("$")
+
+        prefix = process[0]
+        suffix = process[1]
+
+        if len(suffix) == 0:
+            sequence = prefix[-1]+"$"+prefix[0:len(prefix)-1]
+            print(sequence)
+            combinations.append(sequence)
+            combinations_recurs(sequence, combinations)
+
+        elif len(suffix) >= 1 and len(prefix) >= 1:
+
+            sequence = suffix[-1]+prefix[:]+"$"+suffix[:len(suffix)-1]
+            print(sequence)
+            combinations.append(sequence)
+            combinations_recurs(sequence, combinations)
+
+
+
+
+        return combinations
+
+def BWT(file):
+    """
+    Arguments: Takes a nucleotide file
+    Description: Do a Burrows Weeler Compression/Transformation
+    Returns: A file with the compressed sequence
+    """
+
+    document = open(file, "r") #Open the file containing sequence
+
+    raw_sequence = document.readlines()
+    raw_sequence.append("$")
+    sequence = ''.join(raw_sequence)
+    combinations = []
+    return combinations_recurs(sequence, combinations)
+
+
+
+print(os.chdir("/Users/mirandolaleonardo/github/Algo_project"))
+print(BWT("test.txt"))
--- main.py
+++ main.py
@@ -2,5 +2,33 @@
 # -*- coding: utf-8 -*-
 
 
-from Tkinter import Entry, Label, StringVar, Tk , Button, END
+from Tkinter import Entry, Label, StringVar, Tk , Button, END, Radiobutton, Checkbutton, IntVar
 from tkFileDialog import askopenfilename, asksaveasfile
+
+def openfile():
+    """
+    Arguments: None
+    Description: Open a file containing a nucleotide sequence
+    Returns: None
+    """
+    filename = askopenfilename(parent=root)
+    file = open(filename)
+    lines = file.readlines()
+
+root = Tk()
+root.title("FANCY ALGO LEO")
+root.geometry("200x200")
+
+test = IntVar()
+rad_1 = Radiobutton(root, text='BWT', value=0, variable = test)
+rad_2 = Radiobutton(root, text='Hufman', value=0, variable = test)
+rad_3 = Radiobutton(root, text='BWT + Huffman', value=0, variable = test)
+rad_4 = Checkbutton(root, text='Verbose')
+
+rad_1.grid(row=0, column = 0)
+rad_2.grid(row=1, column = 0)
+rad_3.grid(row=2, column = 0)
+rad_4.grid(row=0, column = 2)
+
+
+root.mainloop()
